146b0eb354d13a0fd27d5c35d6a47f51076e6df0
xwj1427698750 <17352959682@163.com>
2020-12-03 22:29:07 +0800

    去除竞标项目列表中不应该出现的操作

--- src/view/desk/myBid/myBid.vue
+++ src/view/desk/myBid/myBid.vue
@@ -80,14 +80,6 @@
                         <i class="el-icon-search"></i>
                         查看
                     </router-link>
-                    <router-link to v-if="!scope.row.status">
-                        <i class="icon iconfont icon-tuandui"></i>
-                        {{scope.row.sele}}
-                    </router-link>
-                    <router-link to v-if="!scope.row.status">
-                        <i class="el-icon-edit"></i>
-                        {{scope.row.edit}}
-                    </router-link>
                     <router-link
                             @click.native="getScore(scope.row.id,scope.row.isScore)"
                             to
@@ -107,19 +99,13 @@
                         <i class="el-icon-delete"></i>
                         删除
                     </router-link>
-                    <router-link
-                            :to="{ path: 'taskIndex', query: { projectId: scope.row.id } }"
-                            v-if="scope.row.status===2"
-                    >
-                        <i class="el-icon-edit"></i>
-                        编辑任务
-                    </router-link>
-                    <router-link to v-if="!scope.row.status">
-                        <i class="el-icon-error"></i>
-                        {{scope.row.ban}}
-                    </router-link>
-
-
+<!--                    <router-link-->
+<!--                            :to="{ path: 'taskIndex', query: { projectId: scope.row.id } }"-->
+<!--                            v-if="scope.row.status===2"-->
+<!--                    >-->
+<!--                        <i class="el-icon-edit"></i>-->
+<!--                        编辑任务-->
+<!--                    </router-link>-->
                     <router-link :to="{path:'projectDeliver',query:{Id:scope.row.id,projectId:scope.row.projectId}}"
                                  v-if="scope.row.status===2">
                         <i class="el-icon-success"></i>
