160830675e6eaa8af33c0171a63e5673173f97dc
Daisy0515 <809713759@qq.com>
2020-12-17 15:49:42 +0800

    任务管理/项目文件/项目成员

---
+++ src/view/desk/projectManagement/projectManageContent/issuesManagement.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "issuesManagement"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/view/desk/projectManagement/projectManageContent/taskManage.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "taskManage"
+    }
+</script>
+
+<style scoped>
+
+</style>
