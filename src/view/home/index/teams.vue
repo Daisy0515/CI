a3ba91e1a7b0fe7df48236a0df2e81605d33e4ef
Qingji <renqjhit@163.com>
2021-02-25 22:24:52 +0800

    TEAM ITEMS头像，TOOL描述

--- src/view/home/index/teams.vue
+++ src/view/home/index/teams.vue
@@ -2,7 +2,7 @@
     <div class="ttoolset">
         <router-link :to="{path:'applicationAccession', query:{id:item.id}}" v-for="(item, index) in plazaList" :key="index">
             <div style=" vertical-align:middle; margin-top:8%">
-                <img src="./icon.png"
+                <img :src="item.headurl"
                      style="width:50px; height:50px; border-radius:50%; margin-left: 5%; ">
                 <div style="display: inline-block; margin-left:15px;">
                     <div style="margin-bottom: 10px">
@@ -46,23 +46,28 @@ export default {
         };
     },
     created: function () {
-        httpGet("/v1/public/share/get/top").then(results => {
-            const {httpCode, data, msg} = results.data;
-            if (httpCode === 200) {
-                this.toolsetList = data;
-            } else if (httpCode !== 401) {
-                errTips(msg);
-            }
-        });
         this.getTeams();
     },
     methods: {
+        checkImgExists(imgUrl) {
+            return new Promise(function(resolve, reject) {
+                var ImgObj = new Image();
+                ImgObj.src = imgUrl;
+                ImgObj.onload = function(res) {
+                    resolve(res);
+                }
+                ImgObj.onerror = function(err) {
+                    reject(err)
+                }
+            })
+        },
         getTeams(val = this.pageData) {
             this.loading = true;
             httpGet("/v1/public/bid/search/getteaminfo", val).then(results => {
                 let getData = results.data;
                 if (getData.httpCode === 200) {
                     this.plazaList = [...getData.data.teamGroupInfoList].slice(0, 5);
+                    console.log(58, this.plazaList);
                     this.pageData.pageNo = getData.data.pageNo;
                     this.totalPage = parseInt(getData.data.totalPage + "0");
                     for (let i of this.plazaList) {
@@ -72,6 +77,11 @@ export default {
                     this.tipData = false;
                     Object.assign(this.pageData, val);
                     for(let i=0; i<5; i++){
+                        this.checkImgExists(this.plazaList[i].headurl).then(()=>{
+
+                        }).catch(()=>{
+                            this.plazaList[i].headurl = require("./icon.png");
+                        })
                         if(this.plazaList[i].teamName!==null && this.plazaList[i].teamName.length > 15){
                             this.plazaList[i].teamName = this.plazaList[i].teamName.slice(0, 15) + "……";
                         }
