4468aabc0ae2f7c024a10c779e9887201047c660
xwj1427698750 <17352959682@163.com>
2021-02-26 19:09:17 +0800

    修复合并产生的冲突

---
+++ src/view/home/index/newindex.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <NewIndex v-if="userToken === null || userToken === ''"/>
+        <Developer v-if="userToken !== null && userToken !== '' && projectRole == 3"/>
+        <Manager v-if="userToken !== null && userToken !== '' && projectRole == 2"/>
+        <IndexForDemand v-if="userToken !== null && userToken !== '' && projectRole == 1"/>
+    </div>
+</template>
+
+<script>
+    import Developer from '@/view/home/index/developer.vue';
+    import Manager from '@/view/home/index/manager.vue';
+    import NewIndex from '@/view/home/index/index.vue';
+    import IndexForDemand from '@/view/home/index/indexfordemand.vue';
+
+    export default {
+        components: {
+            Developer,
+            Manager,
+            NewIndex,
+            IndexForDemand,
+        },
+        data() {
+            return {
+                projectRole: 0,
+                userToken: "",
+            }
+        },
+        created() {
+            this.userToken = sessionStorage.getItem("userToken");
+            this.projectRole = sessionStorage.getItem("projectRole");
+            console.log(32, this.userToken);
+        }
+    }
+</script>
