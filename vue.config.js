92913b03ed4960bb1e7ebe8979be63b4d6efb276
xwj1427698750 <17352959682@163.com>
2021-03-05 21:27:19 +0800

    修改打包方式，增加压缩设置，减少首页加载时间

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+const CompressionPlugin = require("compression-webpack-plugin")
 
 let publicPath = "/";
 if(process.env.NODE_ENV==="production"){
@@ -8,5 +9,19 @@ if(process.env.NODE_ENV==="production"){
     }
 }
 module.exports = {
-    publicPath:  publicPath
+    publicPath:  publicPath,
+    productionSourceMap: false,
+    configureWebpack:config=>{
+        if(process.env.NODE_ENV==="production"){
+            return {
+                plugins:[
+                    new CompressionPlugin({
+                        test: /\.(js|css)(\?.*)?$/i,//需要压缩的文件正则
+                        threshold: 5120,//文件大小大于这个值时启用压缩
+                        deleteOriginalAssets: false//压缩后保留原文件
+                    })
+                ]
+            }
+        }
+    }
 }
