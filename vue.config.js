2bd8e822803b9507f2ea33debd2bd247143b860b
xwj1427698750 <17352959682@163.com>
2021-03-05 15:55:12 +0800

    修改打包方式，原有打包方式加载时间很长

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
 
 let publicPath = "/";
-if(process.env.NODE_ENV==="test"){
-    publicPath = "/cosine/dist";
-}else if(process.env.NODE_ENV==="production"){
-    publicPath = "./"
+if(process.env.NODE_ENV==="production"){
+    if(process.env.VUE_APP_CURRENTMODE === "test"){
+        publicPath = "/cosine/dist";
+    }else if(process.env.VUE_APP_CURRENTMODE === "production"){
+        publicPath = "./"
+    }
 }
 module.exports = {
     publicPath:  publicPath
