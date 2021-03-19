d1023731bee60d2ce3972b5fbd7eb79e64708048
Qingji <renqjhit@163.com>
2021-03-19 10:56:53 +0800

    删除robot

--- src/view/home/index/newindex.vue
+++ src/view/home/index/newindex.vue
@@ -1,17 +1,5 @@
 <template>
     <div>
-        <div id="ROBOT">
-            <div style="position: fixed;z-index: 9999;right: 0;top: 800px;">
-                <img src="./ROBOT.gif" v-on:click="robotDialog=true;">
-            </div>
-        </div>
-        <div id="robot_dialog" v-show="robotDialog">
-            <div style="position:fixed;top:200px;width: 900px;height: 600px;border: 1px solid #dfdfdf;overflow: hidden;
-            left: 50%;margin-left: -450px;box-shadow: 0 0 15px #555;z-index: 9999;background-color: white;">
-                <a class="close" style="position: absolute;right:15px;top:15px;z-index: 9999" v-on:click="robotDialog=false;"></a>
-                <Robot></Robot>
-            </div>
-        </div>
         <Index v-if="userToken === null || userToken === ''"/>
         <Developer v-if="userToken !== null && userToken !== '' && projectRole == 3"/>
         <Manager v-if="userToken !== null && userToken !== '' && projectRole == 2"/>
@@ -24,8 +12,6 @@
     import Manager from '@/view/home/index/manager.vue';
     import Index from '@/view/home/index/index.vue';
     import IndexForDemand from '@/view/home/index/indexfordemand.vue';
-    import ToolSet from '@/view/home/index/newtoolset2.vue';
-    import Robot from '@/view/home/index/robot.vue';
 
 export default {
     components: {
@@ -33,8 +19,6 @@ export default {
         Manager,
         Index,
         IndexForDemand,
-        ToolSet,
-        Robot,
     },
     data() {
         return {
